80a8534a361f5d28acd68a9616a82f9cdf852f48
Aung Myo Kyaw <amk.maeu@gmail.com>
2017-02-13 15:31:24 +0630

    getRequestedBookList offeredBookList approveTrade

--- server/controller/requestController.js
+++ server/controller/requestController.js
@@ -1,4 +1,5 @@
 const Request = require('../model/request');
+const UserBook = require('../model/userBook');
 
 function requestBook(req,res){
   let requesterID = req.user._id;
@@ -35,6 +36,116 @@ function requestBook(req,res){
   })
 }
 
+function getRequestedBookList(req,res){
+  let userID = req.user._id;
+  let offset = req.query.offset || 0;
+  let limit = req.query.limit || 10;
+
+  let numberOfBook,pageCount,currentPage;
+
+  Request.find({
+    ownerID : userID,
+    approved : false
+  })
+  .count()
+  .then(count=>{
+    numberOfBook = count;
+    pageCount = Math.ceil(numberOfBook/limit);
+    currentPage = (offset/limit)+1;
+    return Request.find({
+              ownerID : userID,
+              approved : false
+            })
+            .skip(Number(offset))
+            .limit(Number(limit))
+            .lean()
+  })
+  .then(requestedBooks=>{
+    res.status(200).json({
+      numberOfBook :numberOfBook,
+      pageCount :pageCount,
+      currentPage :currentPage,
+      books: requestedBooks
+    });
+  })
+  .catch(err=>{
+    res.status(500).json({err:err.message});
+  })
+}
+
+function offeredBookList(req,res){
+  let userID = req.user._id;
+  let offset = req.query.offset || 0;
+  let limit = req.query.limit || 10;
+
+  let numberOfBook,pageCount,currentPage;
+
+  Request.find({
+    requesterID : userID,
+    approved : false
+  })
+  .count()
+  .then(count=>{
+    numberOfBook = count;
+    pageCount = Math.ceil(numberOfBook/limit);
+    currentPage = (offset/limit)+1;
+    return Request.find({
+              requesterID : userID,
+              approved : false
+            })
+            .skip(Number(offset))
+            .limit(Number(limit))
+            .lean()
+  })
+  .then(offeredBooks=>{
+    res.status(200).json({
+      numberOfBook :numberOfBook,
+      pageCount :pageCount,
+      currentPage :currentPage,
+      books: offeredBooks
+    });
+  })
+  .catch(err=>{
+    res.status(500).json({err:err.message});
+  });
+}
+
+function approveTrade(req,res){
+  let requestId = req.params.requestID;
+  let newUserBook;
+
+  Request.findOneAndUpdate({_id:requestId},{approved:true},{new:true})
+    .then(request=>{
+      newUserBook = new UserBook({
+        userId : request.requesterID,
+        bookId : request.bookID,
+        isOwner : false
+      });
+      return UserBook.find({
+        userId : request.requesterID,
+        bookId : request.bookID,
+        isOwner : false
+      })
+      .count()
+    })
+    .then(count=>{
+      if(!count){
+        return newUserBook.save();
+      } else {
+        res.sendStatus(202);
+      }
+    })
+    .then(newUserBook=>{
+      res.status(200).json(newUserBook);
+    })
+    .catch(err=>{
+      res.status(500).json({err:err.message});
+    });
+}
+
 module.exports = {
-  requestBook
+  requestBook,
+  getRequestedBookList,
+  offeredBookList,
+  approveTrade
 }
--- server/route/request.js
+++ server/route/request.js
@@ -4,4 +4,13 @@ const request = require('../controller/requestController');
 router.route('/request/:bookID')
       .post(request.requestBook);
 
+router.route('/requested/book')//owner
+      .get(request.getRequestedBookList);
+
+router.route('/offered/book')//requester
+      .get(request.offeredBookList);
+
+router.route('/approve/book/:requestID')
+      .put(request.approveTrade);
+
 module.exports = router;
